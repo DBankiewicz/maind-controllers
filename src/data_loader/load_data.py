e0192c84436c2ad9f64a25a6c8bfe0fb31be3f4d
Tutorial Account - tutorial014 <tutorial014@login01.athena.cyfronet.pl>
2025-11-29 15:02:05 +0100

    refactor

--- src/data_loader/load_data.py
+++ src/data_loader/load_data.py
@@ -1,31 +1,28 @@
 from openai import OpenAI
 
 from src.data_loader.mail import Email
+from src.llm_api.api import get_response
 
 
 
-def extract_data(client, model,  text): 
-    response =  client.chat.completions.create(
-    model=model,
-     messages=[{"role": "user", "content": text}], 
-    )
-    return response.choices[0].message.content
+def extract_data(model,  text): 
+    return get_response(text, model=model)
 
 
-def process_mail(mail: str, client: OpenAI) -> Email:
+def process_mail(mail: str) -> Email:
     # from 
-    from_ = extract_data(client, "meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail addresses of sender: {mail}. This should be one email (usually after 'OD:'). Only output one mail address, no additional text") 
+    from_ = extract_data("meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail addresses of sender: {mail}. This should be one email (usually after 'OD:'). Only output one mail address, no additional text") 
     # to 
-    to_ = extract_data(client, "meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail addresses of recipents: {mail}. This should be at least one email (usually after 'DO:') at the top of the content.  Only output mail addreeses, no additional text")
+    to_ = extract_data("meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail addresses of recipents: {mail}. This should be at least one email (usually after 'DO:') at the top of the content.  Only output mail addreeses, no additional text")
     to_ = to_.split("\n")
     # topic 
-    topic = extract_data(client, "meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail main topic: {mail}. This should be a field called 'Temat'. Only output mail title, no additional text")
+    topic = extract_data("meta-llama/Llama-3.3-70B-Instruct", f"Wrtie mail main topic: {mail}. This should be a field called 'Temat'. Only output mail title, no additional text")
     # data
     data = mail 
     # summary 
-    summary = extract_data(client, "meta-llama/Llama-3.3-70B-Instruct", f"Summarize mail content in 3 sentences : {mail}. Only output summary, no additional text")
+    summary = extract_data("meta-llama/Llama-3.3-70B-Instruct", f"Summarize mail content in 3 sentences : {mail}. Only output summary, no additional text")
     # etra 
-    tags = extract_data(client, "meta-llama/Llama-3.3-70B-Instruct", f"Wrtie potential tags, categroies: {summary}. Only output categories separated by semicolon, no additional text")
+    tags = extract_data("meta-llama/Llama-3.3-70B-Instruct", f"Wrtie potential tags, categroies: {summary}. Only output categories separated by semicolon, no additional text")
 
     extra = {"tags": tags}
     e = Email(from_=from_, to=to_, topic=topic, data=data, summary=summary, extra=extra)
