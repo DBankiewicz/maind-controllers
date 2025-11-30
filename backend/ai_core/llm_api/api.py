b20e9c5142cd3b6f9c850ce2123156014d7c0786
Pavlo <pavlo.pasko.main@gmail.com>
2025-11-30 12:22:46 +0100

    Fix circular import

--- backend/ai_core/llm_api/api.py
+++ backend/ai_core/llm_api/api.py
@@ -9,27 +9,11 @@ from backend.schemas.mail import EmailWithAnalysis
 sys.modules['sqlite3'] = pysqlite3
 
 from chromadb import Collection
-from openai import OpenAI
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-def get_response(prompt: str, model: str = "meta-llama/Llama-3.3-70B-Instruct") -> str:
-    client = OpenAI(
-        api_key=os.getenv("LLM_API_KEY"), 
-        base_url="https://llmlab.plgrid.pl/api/v1"
-    )
-    response =  client.chat.completions.create(
-        model=model,
-        messages=[{"role": "user", "content": prompt}],
-        # input=f"Znajdz tytul maila" # {m}"
-    )
-    res =  response.choices[0].message.content
-    if res is None:
-        res = ''
-    return res
 
 async def async_get_response(prompt: str, model: str = "meta-llama/Llama-3.3-70B-Instruct") -> str:
     client = AsyncOpenAI(
---
+++ backend/ai_core/llm_api/helper.py
@@ -0,0 +1,21 @@
+import openai
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_response(prompt: str, model: str = "meta-llama/Llama-3.3-70B-Instruct") -> str:
+    client = OpenAI(
+        api_key=os.getenv("LLM_API_KEY"), 
+        base_url="https://llmlab.plgrid.pl/api/v1"
+    )
+    response =  client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        # input=f"Znajdz tytul maila" # {m}"
+    )
+    res =  response.choices[0].message.content
+    if res is None:
+        res = ''
+    return res
