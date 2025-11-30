7adbb5a62b92e88bc14cba13ed3b42c3a4c9c51f
Pavlo <pavlo.pasko.main@gmail.com>
2025-11-30 11:01:11 +0100

    Add timestamp to analysis and endpoints for getting emails

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .auth import UserAuth
 from .passport import CookieObj, UserIDObj, PassportObj, LoginResponse
 from .user import UserOut, UserUpdate
-from .mail import EmailIn, EmailAnalysisSchema
+from .mail import EmailIn, EmailAnalysisSchema, EmailWithAnalysis, EmailOut
 from .group import GroupOut, GroupCreate
